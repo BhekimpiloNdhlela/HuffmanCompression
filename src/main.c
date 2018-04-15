4b8138367d04be9ff6dc7e67369bf6f98a317b2d
Bhekimpilo <18998712@sun.ac.za>
2018-04-15 15:35:41 +0200

    indent the C code to two [spaces]

--- src/main.c
+++ src/main.c
@@ -11,7 +11,6 @@ void make_huffman_code(HuffmanNode *, char **);
 static unsigned int total_chars = 0;    /* the total number of chars in the input file */
 static unsigned int number_chars = 0;   /* the number of distinct  chars in input file */
 
-
 /**
  * The Huffman compressor client, calls the get_char_frequency and other relevant functions before
  * initiating the compression process.
@@ -19,13 +18,12 @@ static unsigned int number_chars = 0;   /* the number of distinct  chars in inpu
  * @param[in] argc    the length of the std in
  * @param[in] argv    the input file
  */
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-	int frequency[MAX_HEAP_SIZE] = { 0 };
-	set_char_frequency(argv[1], frequency);
-	return EXIT_SUCCESS;
+  int frequency[MAX_HEAP_SIZE] = { 0 };
+  set_char_frequency(argv[1], frequency);
+  return EXIT_SUCCESS;
 }
-
 
 /**
  * Makes the char frequency table with the aid of a struct defined in the utilities.h
@@ -37,18 +35,16 @@ int main(int argc, char **argv)
  */
 void set_char_frequency(const char *input_file_name, int *frequency)
 {
-	FILE *input_file = open_file(input_file_name, "r");
-    unsigned int c;
-	while ((c = fgetc(input_file)) != EOF) {
-		total_chars  = total_chars + 1;
-		frequency[c] = frequency[c] + 1;
-		number_chars = (frequency[c] == 1) ? number_chars + 1 : number_chars;
-	}
-
-	fclose(input_file);
-	compress(frequency, input_file_name);
+  FILE *input_file = open_file(input_file_name, "r");
+  unsigned int c;
+  while ((c = fgetc(input_file)) != EOF) {
+    total_chars  = total_chars + 1;
+    frequency[c] = frequency[c] + 1;
+    number_chars = (frequency[c] == 1) ? number_chars + 1 : number_chars;
+  }
+  fclose(input_file);
+  compress(frequency, input_file_name);
 }
-
 
 /**
  * The heart of the Huffman compressor, initiates the compression of the input filie and
@@ -58,31 +54,30 @@ void set_char_frequency(const char *input_file_name, int *frequency)
  */
 void compress(int *char_freq, const char *in_name)
 {
-	HuffmanNode *t = emalloc(sizeof(HuffmanNode) * MAX_HEAP_SIZE);
-    char *fmap[MAX_HEAP_SIZE];
-	HeapNode *HN;
-	Heap H;
-    int i;
-    /* initialise the heap and also the table and the huffman tree. */
-	c_heap_initialize(&H);
+  HuffmanNode *t = emalloc(sizeof(HuffmanNode) * MAX_HEAP_SIZE);
+  char *fmap[MAX_HEAP_SIZE];
+  HeapNode *HN;
+  Heap H;
+  int i;
+  /* initialise the heap and also the table and the huffman tree. */
+  c_heap_initialize(&H);
 
-	for (i = 0; i < MAX_HEAP_SIZE; i++) {
-		if (char_freq[i] != 0) { 
-        	HN = emalloc(sizeof(HeapNode));
-            HN->frequency = char_freq[i];
-            HN->c = i;
-            c_heap_insert(&H, HN);
-    	}
-	}
+  for (i = 0; i < MAX_HEAP_SIZE; i++) {
+    if (char_freq[i] != 0) {
+      HN = emalloc(sizeof(HeapNode));
+      HN->frequency = char_freq[i];
+      HN->c = i;
+      c_heap_insert(&H, HN);
+    }
+  }
+  huffman_initialize_table(t);
+  c_huffman_build_tree(&H, &HN);
+  c_huffman_build_table(HN, t, 0, 0);
 
-	huffman_initialize_table(t);
-    c_huffman_build_tree(&H, &HN);
-    c_huffman_build_table(HN, t, 0, 0);
-
-    /* making the huffman encoding with the aid of dec_to_bin from utilities.c */ 	
-	for ( i = 0; i < MAX_HEAP_SIZE; i++) 
-        fmap[i] = (t[i].bit_size != 0) ? dec_to_bin(t[i].huffman_code, t[i].bit_size) : '\0';
-	
-	write_compressed_file(in_name, fmap, total_chars, number_chars, char_freq);
-    free(t);
+  /* making the huffman encoding with the aid of dec_to_bin from utilities.c */
+  for ( i = 0; i < MAX_HEAP_SIZE; i++) {
+    fmap[i] = (t[i].bit_size != 0) ? dec_to_bin(t[i].huffman_code, t[i].bit_size) : '\0';
+  }
+  write_compressed_file(in_name, fmap, total_chars, number_chars, char_freq);
+  free(t);
 }
--- src/utilities.c
+++ src/utilities.c
@@ -8,16 +8,15 @@
  */
 char *dec_to_bin(int n, int size)
 {
-    int k, j, i = 0;
-    char *code = emalloc(size);
-    for (j = size ; j >= 0; j--)  {
-        k = n >> j;
-        code[i++] = (k & 1) ? 1 + '0': 0 + '0';
-    }
-    code[i] = '\0';
-    return  code;
+  int k, j, i = 0;
+  char *code = emalloc(size);
+  for (j = size ; j >= 0; j--)  {
+    k = n >> j;
+    code[i++] = (k & 1) ? 1 + '0': 0 + '0';
+  }
+  code[i] = '\0';
+  return  code;
 }
-
 
 /**
  * emalloc equivalent, dont have to check if memory was allocated successfully
@@ -28,14 +27,13 @@ char *dec_to_bin(int n, int size)
  */
 void *emalloc(size_t size)
 {
-	void *mem_address;
-	if ((mem_address = malloc(size)) == NULL) {
-		fprintf(stderr,"error: malloc of %lu bytes failed:", size);
-		exit(EXIT_FAILURE);
-	}
-	return mem_address;
+  void *mem_address;
+  if ((mem_address = malloc(size)) == NULL) {
+    fprintf(stderr,"error: malloc of %lu bytes failed:", size);
+    exit(EXIT_FAILURE);
+  }
+  return mem_address;
 }
-
 
 /**
  * open file and then check if it was opened successfully. the client does not have
@@ -47,17 +45,16 @@ void *emalloc(size_t size)
  */
 FILE *open_file(const char *filename, const char *mode)
 {
-	FILE *file_ptr;
-	if ((file_ptr = fopen(filename, mode)) == NULL) {
-		fprintf(stderr, "error: could not read file: <%s>\n", filename);
-		exit(EXIT_FAILURE);
-	}
-	return file_ptr;	
+  FILE *file_ptr;
+  if ((file_ptr = fopen(filename, mode)) == NULL) {
+    fprintf(stderr, "error: could not read file: <%s>\n", filename);
+    exit(EXIT_FAILURE);
+  }
+  return file_ptr;
 }
 
-
 /**
- * the file that writes the compressed file. 
+ * the file that writes the compressed file.
  *
  * @param[in]  in_name   name of input file.
  * @param[in]  fmap 	 the map with the huffman codes.
@@ -67,25 +64,21 @@ FILE *open_file(const char *filename, const char *mode)
  */
 void write_compressed_file(const char *in_name, char **fmap, int tchars, int nchars, int *freq)
 {
-	unsigned int i, c;
-	FILE *inf, *of;
-    char out_name[100];
-	strcpy(out_name, in_name);
-	strcat(out_name, ".cz\0");
-    of = open_file(out_name, "wb");
-
-	fwrite(&nchars, 1, 4, of);
-    for (i = 0; i < 256; i++){ 
-		if (freq[i] == 0) continue;
-		fwrite(&i, 1, 1 , of);
-		fwrite(&freq[i], 1, 4 , of);
-    }
-
-    inf = open_file(in_name, "r");
-	while ((c = fgetc(inf)) != EOF) { 
-		fwrite(fmap[c], 1, strlen(fmap[c]), of);
-    }
-	
-    fclose(inf);
-    fclose(of);
+  unsigned int i, c;
+  FILE *inf, *of;
+  char out_name[100];
+  strcpy(out_name, in_name);
+  strcat(out_name, ".cz\0");
+  of = open_file(out_name, "wb");
+  fwrite(&nchars, 1, 4, of);
+  for (i = 0; i < 256; i++) {
+    if (freq[i] == 0) continue;
+    fwrite(&i, 1, 1 , of);
+    fwrite(&freq[i], 1, 4 , of);
+  }
+  inf = open_file(in_name, "r");
+  while ((c = fgetc(inf)) != EOF)
+    fwrite(fmap[c], 1, strlen(fmap[c]), of);
+  fclose(inf);
+  fclose(of);
 }
